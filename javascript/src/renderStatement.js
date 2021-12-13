fa76f23fad772bebb04c19df3980e4dae006430d
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2021-12-12 18:17:42 -0600

    added html rendering as per 'proposed' change jic

--- javascript/src/renderStatement.js
+++ javascript/src/renderStatement.js
@@ -1,11 +1,33 @@
 function renderStatement(statementData) {
-    let result = `Statement for ${statementData.customer}\n`;
-    result += renderPerformances(statementData.performancesInfo);
-    result += `Amount owed is ${getCurrencyFormatted(statementData.totalAmount)}\n`;
-    result += `You earned ${statementData.volumeCredits} credits\n`;
-    return result;
+    return renderAsText(statementData);
+    // return renderAsHTML(statementData);
 
-    function renderPerformances(perfs) {
+    function renderAsText(data) {
+        let result = `Statement for ${data.customer}\n`;
+        result += renderPerformancesAsText(data.performancesInfo);
+        result += `Amount owed is ${getCurrencyFormatted(data.totalAmount)}\n`;
+        result += `You earned ${data.volumeCredits} credits\n`;
+        return result;
+    }
+
+    function renderAsHTML(data){
+        let result = `<h1>Statement for ${data.customer}</h1><br>\n`;
+        result += renderPerformancesAsHTML(data.performancesInfo);
+        result += `<h3>Amount owed is ${getCurrencyFormatted(data.totalAmount)}</h3><br>\n`;
+        result += `<h3>You earned ${data.volumeCredits} credits<h3><br>\n`;
+        return result;
+    }
+    function renderPerformancesAsHTML(perfs) {
+        let result = "<table><th>Name</th><th>Amount</th<th><Audience/th>\n";
+        for (let perf of perfs) {
+            result += `<tr><td> ${perf.play.name} </td><td> ${getCurrencyFormatted(perf.amount)} </td><td> (${
+                perf.audience
+            } seats)</td><td></tr>\n`;
+        }
+        return result;
+    }
+
+    function renderPerformancesAsText(perfs) {
         let result = "";
         for (let perf of perfs) {
             result += ` ${perf.play.name}: ${getCurrencyFormatted(perf.amount)} (${
